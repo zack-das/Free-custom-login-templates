65dd6bb1c59c4afbbb4b0d2ea7b7ce3b0c60627f
zack-das <izackdas@gmail.com>
2025-10-19 11:10:47 +0300

    Updates

--- src/Logins/Login1/Login.jsx
+++ src/Logins/Login1/Login.jsx
@@ -1,18 +1,49 @@
-import React from 'react'
-import './Login.css'
+    import React, { useState } from 'react'
+    import './Login.css'
+    import { FaEye,FaRegEyeSlash} from 'react-icons/fa';
 
     function LoginOne(){
+          const [formData, setFormData] = useState({
+        email: '',
+        password: ''
+    });
+            const [showPassword, setShowPassword] = useState(false);
+
+            const handleChange = (e) => {
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.value
+        });
+    };
+
+    const togglePasswordVisibility = () => {
+        setShowPassword(!showPassword);
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log('Form submitted:', formData);
+    };
+
     return(
         <div className="Login-container">
            <div className="form-container">
                 <form className="" action="">
                     <h1>Login</h1>
-                    <br/><br/>
                 <label>Email</label>
                 <input type="email" name="Email" id="email"/>
                 <br/><br/>
                 <label>Password</label>
-                <input type="Password" name="Password" id="password"/>
+                <input type={showPassword ? "text" : "password"}
+                 onChange={handleChange}
+                    name="Password" id="password"/>
+                <button
+                    type="button"
+                    onClick={togglePasswordVisibility}
+                    className="toggle-password"
+                >
+                    {showPassword ? <FaRegEyeSlash/> : <FaEye />}
+                </button>
                 <div className="altcreateAcc">
                     <button>Login</button>
                     <h2>OR</h2>
